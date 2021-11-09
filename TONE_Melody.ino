2cfec61867309b8435e6f89b4058886062a66838
candy0710 <83740906+candy0710@users.noreply.github.com>
2021-11-09 11:50:16 +0800

    Add files via upload

---
+++ TONE_Melody.ino
@@ -0,0 +1,76 @@
+//#include "pitches.h"
+#define NOTE_D0 -1 
+#define NOTE_D1 294 
+#define NOTE_D2 330 
+#define NOTE_D3 350 
+#define NOTE_D4 393 
+#define NOTE_D5 441 
+#define NOTE_D6 495 
+#define NOTE_D7 556 
+
+#define NOTE_DL1 147 
+#define NOTE_DL2 165 
+#define NOTE_DL3 175 
+#define NOTE_DL4 196 
+#define NOTE_DL5 221 
+#define NOTE_DL6 248 
+#define NOTE_DL7 278 
+
+#define NOTE_DH1 589 
+#define NOTE_DH2 661 
+#define NOTE_DH3 700 
+#define NOTE_DH4 786 
+#define NOTE_DH5 882 
+#define NOTE_DH6 990 
+#define NOTE_DH7 112  
+
+int freq = 2000;
+int channel = 0;
+int resolution = 8;
+int length;
+int tonePin = 12;
+
+
+
+int melody[] = {NOTE_D0,NOTE_D0,NOTE_D0,NOTE_D6,NOTE_D7,NOTE_DH1,NOTE_D7,NOTE_DH1,NOTE_DH3,NOTE_D7,NOTE_D7,NOTE_D7,NOTE_D3,NOTE_D3,
+ NOTE_D6,NOTE_D5,NOTE_D6,NOTE_DH1,NOTE_D5,NOTE_D5,NOTE_D5,NOTE_D3,NOTE_D4,NOTE_D3,NOTE_D4,NOTE_DH1,
+ NOTE_D3,NOTE_D3,NOTE_D0,NOTE_DH1,NOTE_DH1,NOTE_DH1,NOTE_D7,NOTE_D4,NOTE_D4,NOTE_D7,NOTE_D7,NOTE_D7,NOTE_D0,NOTE_D6,NOTE_D7,
+ NOTE_DH1,NOTE_D7,NOTE_DH1,NOTE_DH3,NOTE_D7,NOTE_D7,NOTE_D7,NOTE_D3,NOTE_D3,NOTE_D6,NOTE_D5,NOTE_D6,NOTE_DH1,
+ NOTE_D5,NOTE_D5,NOTE_D5,NOTE_D2,NOTE_D3,NOTE_D4,NOTE_DH1,NOTE_D7,NOTE_D7,NOTE_DH1,NOTE_DH1,NOTE_DH2,NOTE_DH2,NOTE_DH3,NOTE_DH1,NOTE_DH1,NOTE_DH1,
+ NOTE_DH1,NOTE_D7,NOTE_D6,NOTE_D6,NOTE_D7,NOTE_D5,NOTE_D6,NOTE_D6,NOTE_D6,NOTE_DH1,NOTE_DH2,NOTE_DH3,NOTE_DH2,NOTE_DH3,NOTE_DH5,
+ NOTE_DH2,NOTE_DH2,NOTE_DH2,NOTE_D5,NOTE_D5,NOTE_DH1,NOTE_D7,NOTE_DH1,NOTE_DH3,NOTE_DH3,NOTE_DH3,NOTE_DH3,NOTE_DH3,
+ NOTE_D6,NOTE_D7,NOTE_DH1,NOTE_D7,NOTE_DH2,NOTE_DH2,NOTE_DH1,NOTE_D5,NOTE_D5,NOTE_D5,NOTE_DH4,NOTE_DH3,NOTE_DH2,NOTE_DH1,
+ NOTE_DH3,NOTE_DH3,NOTE_DH3,NOTE_DH3,NOTE_DH6,NOTE_DH6,NOTE_DH5,NOTE_DH5,NOTE_DH3,NOTE_DH2,NOTE_DH1,NOTE_DH1,NOTE_D0,NOTE_DH1,
+ NOTE_DH2,NOTE_DH1,NOTE_DH2,NOTE_DH2,NOTE_DH5,NOTE_DH3,NOTE_DH3,NOTE_DH3,NOTE_DH3,NOTE_DH6,NOTE_DH6,NOTE_DH5,NOTE_DH5,
+ NOTE_DH3,NOTE_DH2,NOTE_DH1,NOTE_DH1,NOTE_D0,NOTE_DH1,NOTE_DH2,NOTE_DH1,NOTE_DH2,NOTE_DH2,NOTE_D7,NOTE_D6,NOTE_D6,NOTE_D6,NOTE_D6,NOTE_D7
+};
+float noteDurations[] = {1,1,1,0.5,0.5,1+0.5,0.5,1,1,1,1,1,0.5,0.5,
+  1+0.5,0.5,1,1,1,1,1,1,1+0.5,0.5,1,1, 
+  1,1,0.5,0.5,0.5,0.5,1+0.5,0.5,1,1,1,1,1,0.5,0.5,
+  1+0.5,0.5,1,1,1,1,1,0.5,0.5,1+0.5,0.5,1,1,
+  1,1,1,0.5,0.5,1,0.5,0.25,0.25,0.25,0.5,0.5,0.5,0.5,0.25,0.5,1,
+  0.5,0.5,0.5,0.5,1,1,1,1,1,0.5,0.5,1+0.5,0.5,1,1,
+  1,1,1,0.5,0.5,1.5,0.5,1,1,1,1,1,1,
+  0.5,0.5,1,1,0.5,0.5,1.5,0.25,0.5,1,1,1,1,1,
+  1,1,1,1,1,1,1,1,0.5,0.5,1,1,0.5,0.5,
+  1,0.5,0.5,1,1,1,1,1,1,1,1,1,1,
+  0.5,0.5,1,1,0.5,0.5,1,0.5,0.25,0.5,1,1,1,1,0.5,0.5
+};
+
+void setup() {
+  Serial.begin(115200);
+  length = sizeof(melody)/sizeof(melody[0]);
+  ledcSetup(channel, freq, resolution);
+  ledcAttachPin(tonePin, channel);
+}
+
+void loop() {
+    for (int thisNote = 0; thisNote < length; thisNote++) {
+      int noteDuration = noteDurations[thisNote]*230;
+      ledcWriteTone(channel, melody[thisNote]);
+      delay(noteDuration);
+      ledcWriteTone(channel, 0);
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+    }
+}
